3f79b9c22b138faeecb217b1cab0b4617bb79433
MeLook1 <andruxa_099@mail.ru>
2023-07-01 15:36:56 +0300

    прошлый коммит с ошибкой, этот норм

--- api.js
+++ api.js
@@ -103,7 +103,7 @@ export function loginUser({ login, password }) {
 
 //Проверка
 export const isUserAuthorization = () => {
-    const token = localStorage.getItem('0');
+    const token = localStorage.getItem('token');
     if (token) {
         return true;
     }
